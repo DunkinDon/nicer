a9db4e36d48da0970fa133826203cde205cc1646
DunkinDon <150682109+DunkinDon@users.noreply.github.com>
2024-05-03 18:06:04 +0100

    Update and rename text.ts to test.ts

---
+++ nicer-formatter-main/test.ts
@@ -0,0 +1,6 @@
+const x = 1;
+
+function multiply(x: number, iterations: number): number {
+  let total = 0;
+  return total
+};
--- nicer-formatter-main/text.ts
+++
@@ -1,6 +0,0 @@
-const x = 1;
-
-function multiply(x: number, iterations: number) {
-  let total = 0;
-  return total
-};
